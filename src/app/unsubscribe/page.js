f7ceb43441eba023cdc2f14623afeb9506cd7ba7
Irawo <timothyaremu63@gmail.com>
2024-08-15 00:11:45 +0100

    u

--- src/app/unsubscribe/page.js
+++ src/app/unsubscribe/page.js
@@ -3,8 +3,13 @@ import React, { useEffect, useState } from "react";
 import styles from "./unsubscribe.module.css";
 
 const Page = () => {
+  const [userEmail, setUserEmail] = useState("");
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [message, setMessage] = useState("");
+
+  const handleInputChange = (e) => {
+    setUserEmail(e.target.value);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -19,7 +24,7 @@ const Page = () => {
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify({ email: "timothyaremu63@gmail.com" }),
+          body: JSON.stringify({ email: userEmail }),
         }
       );
 
@@ -52,11 +57,19 @@ const Page = () => {
       <form className={styles.form} onSubmit={handleSubmit}>
         <h1 className={styles.head}>Unsubscribe</h1>
         <p className={styles.text}>
+          Kindly enter your email below to unsubscribe:
+        </p>
+        {/* <p className={styles.text}>
           Click on &quot;Unsubscribe&quot; to stop receiving emails from
           Ventmoir on this email address:
-        </p>
-        <center>test302@gmail.com</center>
-
+        </p> */}
+        {/* <center>test302@gmail.com</center> */}
+        <input
+          type="email"
+          placeholder="Your email"
+          onChange={handleInputChange}
+          required
+        />
         <button>Unsubscribe</button>
       </form>
     </main>
